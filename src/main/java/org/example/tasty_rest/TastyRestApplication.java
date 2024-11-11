57584c80a1ec4c01fabbead6e2a90fde48c1e05a
Bhavya <bhavyarjoshi19@gmail.com>
2024-11-12 00:27:20 +0530

    Project Intialize

---
+++ src/main/java/org/example/tasty_rest/TastyRestApplication.java
@@ -0,0 +1,13 @@
+package org.example.tasty_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TastyRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TastyRestApplication.class, args);
+    }
+
+}
